527b66a3d72da440f683b856f07ef9b44ec7c301
VinayJ3579 <156558618+VinayJ3579@users.noreply.github.com>
2024-03-10 23:55:34 -0400

    update

---
+++ ITIS-3135/javaScript/HeaderFooter.js
@@ -0,0 +1,30 @@
+
+document.getElementById("header").innerHTML = `<h1>Vinay Joshi's Virtuous Anaconda|| ITIS 3135</h1>
+                                                   			<nav>
+                                                   				<a href="./">Home</a> ||
+                                                   				<a href="introduction.html">Introduction</a> ||
+                                                   				<a href="contract.html">Contract</a> ||
+                                                   				<a href="website_evaluation.html">Website Evaluation</a>
+                                                   			</nav>`;
+document.getElementById("header").style.textAlign = "center";
+document.getElementById("footer").style.textAlign = "center";
+
+document.getElementById("footer").innerHTML = `<nav>
+                                                   				<a href="https://webpages.charlotte.edu/vjoshi9/" >My Charlotte page</a> ||
+                                                   				<a href="https://vinayj3579.github.io/">GitHub Page</a> ||
+                                                   				<a href="https://github.com/VinayJ3579" >GitHub</a> ||
+                                                   				<a href="https://www.codecademy.com/profiles/dev8751316997">CodeAcademy</a> ||
+                                                   				<a href="https://www.freecodecamp.org/VinayJ3579" >FreeCodeCamp</a> ||
+                                                   				<a href="https://www.linkedin.com/in/vinayjoshi23/" >LinkedIn</a> ||
+                                                   				<a href="https://github.com/VinayJ3579/VinayJ3579.github.io">GitHub repository</a>
+                                                   				<div>
+                                                   					<p>Page built by <a href="/ITIS-3135/virtuousanacondainc.html">Virtuous Anaconda .Inc</a></p>
+                                                   					<a href="https://jigsaw.w3.org/css-validator/validator?uri=https://webpages.charlotte.edu/mhoang4/itis3135/" id="validation_link_css">
+                                                   						<img style="border:0;width:88px;height:31px" src="https://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!">
+                                                   					</a>
+                                                   					<a href="https://validator.w3.org/check?uri=https://webpages.charlotte.edu/mhoang4/itis3135/" id="validation_link_html">
+                                                   						<img style="border:0;width:88px;height:31px" src="https://mytienhoang.github.io/itis3135/z_archives/html_validation.png" alt="Valid HTML!">
+                                                   					</a>
+                                                   				</div>
+                                                   			</nav>`;
+
